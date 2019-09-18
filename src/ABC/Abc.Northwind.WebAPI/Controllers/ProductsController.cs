089a5caf353461eda950c7bde0ab9e68d2371cd9
shaopard <adi_azrael@yahoo.com>
2019-09-18 11:31:02 +0300

    Add Dependency Injection configuration for the WebAPI controller.

---
+++ src/ABC/Abc.Northwind.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿// ------------------------------------------------------------------------------
+//     <copyright file="ProductsController.cs" company="BlackLine">
+//         Copyright (C) BlackLine. All rights reserved.
+//     </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Web.Http;
+
+using Abc.Northwind.Business.Abstract;
+using Abc.Northwind.Entities.ConcreteTypes;
+
+namespace Abc.Northwind.WebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private readonly IProductService c_productService;
+
+        public ProductsController(IProductService productService)
+        {
+            c_productService = productService;
+        }
+
+        public List<Product> Get()
+        {
+            List<Product> products = c_productService.GetAll();
+
+            return products;
+        }
+    }
+}
